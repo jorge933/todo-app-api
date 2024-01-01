4261cbb738aa1c8adc318896a0c565047272675b
Jorge <jorgevitorp9@gmail.com>
2024-01-01 13:05:54 -0300

    📦 refactor(auth.controller): authentication methods abstracted

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
-import { GetUserId } from './decorators/get-user';
 import { IsPublic } from './decorators/is-public-route';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login.dto';
@@ -13,23 +12,28 @@ export class AuthController {
     private readonly jwtService: JwtService,
   ) {}
 
-  @Post()
+  @Post('create-user')
   @IsPublic()
   async createUser(@Body() newUser: CreateUserDto) {
-    const user = await this.authService.create(newUser);
-
-    if (!user) return;
-
-    const token = this.jwtService.sign(user);
-
-    return token;
+    return await this.authenticate('create', newUser);
   }
 
   @Post('login')
   @IsPublic()
   async login(@Body() credentials: LoginUserDto) {
-    const user = await this.authService.login(credentials);
+    return await this.authenticate('login', credentials);
+  }
+
+  async authenticate(
+    methodToCall: string,
+    credentials: CreateUserDto | LoginUserDto,
+  ) {
+    const user = await this.authService[methodToCall](credentials);
+
+    if (!user?.id) return;
+
     const token = this.jwtService.sign(user);
+
     return token;
   }
 }
