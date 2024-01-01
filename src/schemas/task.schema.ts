a3ff396ea9bae7294a15b07022e77f5f7fe71a34
Jorge <jorgevitorp9@gmail.com>
2024-01-01 13:33:52 -0300

    📦 refactor: task name is not unique

--- src/schemas/task.schema.ts
+++ src/schemas/task.schema.ts
@@ -7,7 +7,7 @@ export type TaskDocument = HydratedDocument<Task>;
   collection: 'tasks',
 })
 export class Task {
-  @Prop({ type: String, required: true, unique: true })
+  @Prop({ type: String, required: true })
   name: string;
 
   @Prop({ type: Types.ObjectId, ref: 'User' })
