e091b08f53fecf1a4e604ee7a7cae030a2278fdf
Jorge <jorgevitorp9@gmail.com>
2024-01-02 13:58:46 -0300

    🧪 test(tasks.controller.spec): add scenarios to tasks.controller.spec

--- src/controllers/tasks/tasks.controller.spec.ts
+++ src/controllers/tasks/tasks.controller.spec.ts
@@ -1,18 +1,88 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
+import { ITask } from 'src/interfaces/task';
+import { CreateTaskDto } from '../../services/tasks/create-task.dto';
+import { TasksService } from '../../services/tasks/tasks.service';
 import { TasksController } from './tasks.controller';
 
 describe('TasksController', () => {
-  let controller: TasksController;
+  let tasksController: TasksController;
+  let tasksService: TasksService;
+
+  let tasks: ITask[] = [];
+  const owner = new Types.ObjectId().toString();
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const app: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
-    }).compile();
+      providers: [
+        {
+          provide: TasksService,
+          useValue: {
+            getAll(userId: string) {
+              const tasksOfUser = tasks.filter((task) => task.owner === userId);
 
-    controller = module.get<TasksController>(TasksController);
+              return tasksOfUser;
+            },
+
+            create(owner: string, { name }: CreateTaskDto) {
+              const id = new Types.ObjectId().toString();
+              const task = { _id: id, name, owner };
+              tasks.push(task);
+
+              return id;
+            },
+
+            delete(userId: string, id: string) {
+              const newTasksValue = tasks.filter((task) => {
+                const isTaskToRemove =
+                  (task.owner === userId && task._id !== id) ||
+                  task.owner !== userId;
+                return isTaskToRemove;
+              });
+
+              tasks = newTasksValue;
+            },
+          },
+        },
+      ],
+    }).compile();
+    tasksService = app.get<TasksService>(TasksService);
+    tasksController = app.get<TasksController>(TasksController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should create a task and return id', () => {
+    const { length } = tasks;
+    const task = {
+      name: 'Work',
+    };
+
+    const taskId = tasksController.create(owner, task);
+
+    expect(length + 1).toBe(tasks.length);
+  });
+
+  it('should return all tasks from user', () => {
+    const id = new Types.ObjectId().toString();
+    const userId = new Types.ObjectId().toString();
+    tasks.push({
+      _id: id,
+      name: 'Read',
+      owner: userId,
+    });
+
+    const userTasks = tasksController.getAll(owner) as unknown as ITask[];
+    const expectedUserTasks = [tasks[0]];
+
+    expect(expectedUserTasks).toEqual(userTasks);
+  });
+
+  it('should delete task', () => {
+    const { length } = tasks;
+
+    tasksController.delete(owner, { id: tasks[0]._id });
+
+    const expectedLength = length - 1;
+    expect(expectedLength).toBe(tasks.length);
   });
 });
