48c7cf2d33da3cfed3f86238481f347af1b881bb
Jorge <jorgevitorp9@gmail.com>
2024-01-02 11:05:37 -0300

    🧪 test(auth.controller.spec): add scenarios to auth.controller

---
+++ src/modules/auth/auth.controller.spec.ts
@@ -0,0 +1,72 @@
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
+import { UnitOfWorkModule } from '../unit-of-work/unit-of-work.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login.dto';
+
+fdescribe('AuthController', () => {
+  let authController: AuthController;
+  let authService: AuthService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+        MongooseModule.forRoot(process.env.DB_URI),
+        JwtModule.register({
+          secret: process.env.JWT_SECRET,
+          signOptions: { expiresIn: '10d' },
+        }),
+        UnitOfWorkModule,
+      ],
+      controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            create: jest
+              .fn()
+              .mockImplementation((user: CreateUserDto) =>
+                Promise.resolve({ id: new Types.ObjectId() }),
+              ),
+            login: jest
+              .fn()
+              .mockImplementation((user: LoginUserDto) =>
+                Promise.resolve({ id: new Types.ObjectId() }),
+              ),
+          },
+        },
+      ],
+    }).compile();
+    authService = app.get<AuthService>(AuthService);
+    authController = app.get<AuthController>(AuthController);
+  });
+
+  it('should create and return jwt token string', async () => {
+    const userDto = {
+      username: 'jorge',
+      email: 'jorge@gmail.com',
+      password: 'jorge',
+    };
+
+    const result = await authController.createUser(userDto);
+
+    expect(typeof result).toBe('string');
+  });
+
+  it('should return a jwt token string', async () => {
+    const userCredentials = {
+      login: 'jorge@gmail.com',
+      password: 'jorge',
+    };
+
+    const result = await authController.login(userCredentials);
+
+    expect(typeof result).toBe('string');
+  });
+});
