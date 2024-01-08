8651db09df75101f226a261a3b0bcbd8aee58d86
Jorge <jorgevitorp9@gmail.com>
2024-01-08 14:20:23 -0300

    📦 refactor: unused imports removed

--- src/repositories/tasks/tasks.repository.ts
+++ src/repositories/tasks/tasks.repository.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
-import { FilterQuery, Model, SortOrder } from 'mongoose';
+import { Model } from 'mongoose';
 
 import { Task } from '../../schemas/task.schema';
 
 import { BaseRepository } from '../base/base.repository';
-import { QueryOptions } from 'src/controllers/tasks/tasks.controller';
-import { ITask } from 'src/interfaces/task';
-import { date } from 'src/helpers/date';
-import { like } from 'src/helpers/like';
 
 @Injectable()
 export class TasksRepository extends BaseRepository<Task> {
