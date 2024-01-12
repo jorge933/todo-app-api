ab14a601902fe24a7cc0bb57930e4fd99db9b7a9
Jorge <jorgevitorp9@gmail.com>
2024-01-12 12:37:40 -0300

    ✨ feat(main.ts): listening to the port that is defined in the environment variables

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
     new ResponseInterceptor(await app.resolve(UnitOfWorkService)),
   );
   app.useGlobalPipes(PARAMS_VALIDATION_PIPE);
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
