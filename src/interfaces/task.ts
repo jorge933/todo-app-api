95b167cd6346ff83f928b5a975be2707013e9107
Jorge <jorgevitorp9@gmail.com>
2024-01-24 23:17:35 -0300

    📦 refactor: task interface deleted

--- src/interfaces/task.ts
+++
@@ -1,9 +0,0 @@
-import { TaskPriority } from 'src/enums/task-priority';
-
-export interface ITask {
-  id: number;
-  name: string;
-  owner: number;
-  priority: TaskPriority;
-  completed: boolean;
-}
