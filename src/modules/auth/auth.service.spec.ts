4f11b9b9b6b74862ab39056d2ade48be2c2b8e0d
Jorge <jorgevitorp9@gmail.com>
2024-01-28 13:28:43 -0300

    📦 refactor(auth.service.spec.ts): scenario refactored

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -2,63 +2,158 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login.dto';
-import { Types } from 'mongoose';
+import { HttpStatus } from '@nestjs/common';
+import { HttpTypeErrors } from '../../enums/http-type-errors';
+import { User, UserDocument } from '../../schemas/user.schema';
+import { JwtService } from '@nestjs/jwt';
+import { UnitOfWorkService } from '../unit-of-work/unit-of-work.service';
+import * as bcrypt from 'bcrypt';
+import { FlattenMaps } from 'mongoose';
 
 describe('AuthService', () => {
-  let authService: AuthService;
+  let service: AuthService;
+  let jwtService: JwtService;
+  let mockUnitOfWorkService: UnitOfWorkService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
+        AuthService,
+        JwtService,
         {
-          provide: AuthService,
+          provide: UnitOfWorkService,
           useValue: {
-            create: jest
-              .fn()
-              .mockImplementation((user: CreateUserDto) =>
-                Promise.resolve({ _id: new Types.ObjectId() }),
-              ),
-            login: jest
-              .fn()
-              .mockImplementation((user: LoginUserDto) =>
-                Promise.resolve({ _id: new Types.ObjectId() }),
-              ),
+            userRepository: {
+              findOne: jest.fn(),
+              create: jest.fn(),
+            },
+            domainErrorsService: {
+              addError: jest.fn(),
+            },
           },
         },
       ],
     }).compile();
 
-    authService = module.get<AuthService>(AuthService);
+    service = module.get<AuthService>(AuthService);
+    jwtService = module.get<JwtService>(JwtService);
+    mockUnitOfWorkService = module.get<UnitOfWorkService>(UnitOfWorkService);
   });
 
-  it('should create and return object with user id', async () => {
-    const userDto = {
-      username: 'jorge',
-      email: 'jorge@gmail.com',
-      password: 'jorge',
-    };
-
-    const result = (await authService.create(userDto)) as unknown as {
-      id: Types.ObjectId;
-    };
-    const isId = result.id instanceof Types.ObjectId;
-
-    expect(result).toHaveProperty('id');
-    expect(isId).toBe(true);
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  it('should return object with user id', async () => {
-    const userCredentials = {
-      login: 'jorge@gmail.com',
-      password: 'jorge',
+  it('should create user and return token and user details', async () => {
+    const newUser: CreateUserDto = {
+      email: 'test@example.com',
+      username: 'testuser',
+      password: 'password',
     };
 
-    const result = (await authService.login(userCredentials)) as unknown as {
-      id: Types.ObjectId;
-    };
-    const isId = result.id instanceof Types.ObjectId;
+    const hashedPassword = await bcrypt.hash(newUser.password, 10);
 
-    expect(result).toHaveProperty('id');
-    expect(isId).toBe(true);
+    const user: User = {
+      id: 1,
+      email: newUser.email,
+      username: newUser.username,
+      password: hashedPassword,
+    };
+    const expectUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
+    const token = 'jwt_token';
+
+    jest.spyOn(service, 'create').mockResolvedValue(user as UserDocument);
+    jest.spyOn(jwtService, 'sign').mockReturnValue(token);
+
+    const result = await service.createUser(newUser);
+
+    expect(typeof result.token).toBe('string');
+    expect(result.user).toEqual(expectUser);
+  });
+
+  it('should handle already registered credentials', async () => {
+    const newUser: CreateUserDto = {
+      email: 'test@example.com',
+      username: 'testuser',
+      password: 'password',
+    };
+
+    jest.spyOn(service, 'findOne').mockResolvedValue({
+      id: 1,
+      ...newUser,
+    } as UserDocument);
+
+    await service.createUser(newUser);
+
+    expect(
+      mockUnitOfWorkService.domainErrorsService.addError,
+    ).toHaveBeenCalledWith(
+      {
+        message: 'Estas Credenciais já foram registradas no banco!',
+        type: HttpTypeErrors.ALREADY_BEEN_REGISTERED,
+      },
+      HttpStatus.UNAUTHORIZED,
+    );
+  });
+
+  it('should login user and return token and user details', async () => {
+    const loginUser: LoginUserDto = {
+      login: 'testuser',
+      password: 'password',
+    };
+
+    const hashedPassword = await bcrypt.hash(loginUser.password, 10);
+
+    const user: User = {
+      _id: 1,
+      email: 'test@example.com',
+      username: loginUser.login,
+      password: hashedPassword,
+    };
+
+    jest.spyOn(service, 'findOne').mockResolvedValue({
+      ...user,
+      toJSON: (): FlattenMaps<User> => {
+        const userJson = { ...user, id: user._id };
+        delete userJson._id;
+        return userJson as User;
+      },
+    } as UserDocument);
+
+    jest.spyOn(jwtService, 'sign').mockReturnValue('jwt_token');
+
+    const result = await service.login(loginUser);
+
+    expect(typeof result.token).toBe('string');
+    expect(result.user).toEqual({
+      id: user._id,
+      email: user.email,
+      username: user.username,
+    });
+  });
+
+  it('should handle invalid credentials', async () => {
+    const loginUser: LoginUserDto = {
+      login: 'testuser',
+      password: 'password',
+    };
+
+    jest.spyOn(service, 'findOne').mockResolvedValue(null);
+
+    await service.login(loginUser);
+
+    expect(
+      mockUnitOfWorkService.domainErrorsService.addError,
+    ).toHaveBeenCalledWith(
+      {
+        message: 'Credenciais inválidas!',
+        type: HttpTypeErrors.INVALID_CREDENTIALS,
+      },
+      HttpStatus.UNAUTHORIZED,
+    );
   });
 });
