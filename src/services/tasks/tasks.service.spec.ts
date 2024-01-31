4fc9a26f54200d961d3204971c9da279fb961ce1
Jorge <jorgevitorp9@gmail.com>
2024-01-31 15:35:37 -0300

    🧪 test(tasks.service.spec.ts): scenario added to method editTask

--- src/services/tasks/tasks.service.spec.ts
+++ src/services/tasks/tasks.service.spec.ts
@@ -1,14 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { TasksRepository } from '../../repositories/tasks/tasks.repository';
+import { EditTaskDto } from '../../controllers/tasks/task.dto';
+import { getModelToken } from '@nestjs/mongoose';
+import { Task } from '../../schemas/task.schema';
 
 describe('TasksService', () => {
   let service: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        TasksRepository,
+        { provide: getModelToken(Task.name), useValue: jest.fn() },
+      ],
     }).compile();
 
     service = module.get<TasksService>(TasksService);
+  });
+
+  it('should edit a task successfully', () => {
+    const userId = 1;
+    const taskId = 123;
+    const editTaskDto: Partial<EditTaskDto> = {
+      name: 'Updated Task Name',
+    };
+    const expectedMessage = { message: 'Tarefa atualizada com sucesso!' };
+
+    jest.spyOn(service, 'updateOne').mockImplementation(jest.fn());
+
+    const result = service.editTask(userId, taskId, editTaskDto);
+
+    expect(service.updateOne).toHaveBeenCalledWith(
+      { owner: userId, _id: taskId },
+      editTaskDto,
+    );
+    expect(result).toEqual(expectedMessage);
   });
 });
